252d60b0364f91a78349f93cf92726812d5259d9
Mustafa Johnson <mustafajohnson262@gmail.com>
2026-01-15 16:42:16 -0500

    updated server

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,19 +2,22 @@ import Title from "./components/Title";
 import Nav from "./components/Nav";
 import InputBar from "./components/InputBar";
 
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/* nav components */}
       <nav>
         <Nav />
       </nav>
+      {/* header comnponent area */}
       <header>
         <Title />
       </header>
+      {/* main component area */}
       <main></main>
+      {/* footer component area */}
       <footer>
         <div>
           <InputBar />
