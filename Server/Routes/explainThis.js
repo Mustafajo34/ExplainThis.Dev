618efb99446fa18aefe6d24b9b8ba1c833b8ba2e
Mustafa Johnson <mustafajohnson262@gmail.com>
2026-01-16 09:49:38 -0500

    updated backend route

--- Server/Routes/explainThis.js
+++ Server/Routes/explainThis.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 const validateInput = require("../Middleware/validateInput.js");
 
+router.post("/python", validateInput, async (req, res) => {
+  // input variable that holds req body object
+  const { input } = req.body;
+  // ai object
+  /* 
+  const aiReply = 
+  Generated python code
+  def explain_this():
+    user_input = """${input}"""
+    print("This is your input explained")
+    print(user_input)
+  if __name__ == __main__:
+    explain_input()
 
-router.post("/python", validateInput, async (req, res) => {});
+    res.type("text/plain")
+    res.send(aiReply)
+    */
+});
 
 module.exports = router;
